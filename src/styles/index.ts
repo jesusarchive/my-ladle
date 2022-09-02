a051701eba388df772fb8cb5c2d870e5fe1dfd57
jesusarchive <jbrdev21880949243098@protonmail.com>
2022-09-02 23:07:05 +0200

    clean

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,9 +1,4 @@
-/*
-Copyright (c) Uber Technologies, Inc.
 
-This source code is licensed under the MIT license found in the
-LICENSE file in the root directory of this source tree.
-*/
 import { ThemeContext } from './theme-provider';
 
 export { default as ThemeProvider, type ThemeProviderProps } from './theme-provider';
